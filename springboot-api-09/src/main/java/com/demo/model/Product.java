cffa9512453f721edfab4621702676146bca7069
Haridas250503 <haridasshinde314@gmail.com>
2026-01-28 21:17:30 +0530

    initial project for springboot-framework09

---
+++ springboot-api-09/src/main/java/com/demo/model/Product.java
@@ -0,0 +1,47 @@
+package com.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private double price;
+	
+	@ManyToOne
+	@JoinColumn(name="id",nullable=false)
+	private Category category;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+}
